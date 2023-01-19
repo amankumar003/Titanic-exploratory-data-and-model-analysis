4636a5f729ac5f931d951a899a0823c3c428bc13
Aman kumar <91831652+amankumar003@users.noreply.github.com>
2023-01-19 20:37:13 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,27 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.responses import HTMLResponse, JSONResponse
+from pyrsistent import PClass
+
+from .ml_model import predict_survival_chance
+
+
+class ModelData(BaseModel):
+    Pclass: int
+    Sex: int
+    Age: int
+    SibSp: int
+    Parch: int
+    Fare: float
+    Embarked: str
+
+app = FastAPI()
+
+@app.post('/predict')
+def predict_survival_chance_api(data: ModelData):
+    input_data = data.dict()
+    prediction = predict_survival_chance(input_data)
+    return {'survival_chance': prediction}
 
